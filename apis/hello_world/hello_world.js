21f59ca60be8d1757903a398d8a5d1f2ff3287b6
Mehrdad Eshraghi <67071121+MehrdadEshraghi@users.noreply.github.com>
2021-12-29 13:46:40 +0330

    Update hello_world.js

--- apis/hello_world/hello_world.js
+++ apis/hello_world/hello_world.js
@@ -8,7 +8,7 @@ exports.handler = async function(event, context) {
   "multiValueHeaders": { 
     "X-Custom-Header": ["My value", "My other value"],
   },
-  "body": "byeeeeeeeeeee" + event.name
+  "body": "byeeeeeeeeeee" + event.body.name
 }
 return response
 }
